94877c8771b670082ab54f0f8f294f5a67b3f17c
efmmoncada <emmanuel.moncada27@gmail.com>
2024-02-21 00:16:54 -0800

    remove caching on dev to imporove dx
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- frontend/src/utils/fetchFromPayload.ts
+++ frontend/src/utils/fetchFromPayload.ts
@@ -2,7 +2,7 @@ export default async function fetchFromPayload<T>(urlPath: string): Promise<T> {
   const url = `${process.env.CMS_HOSTNAME}${urlPath}`;
 
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, { cache: process.env.NODE_ENV !== "production" ? 'force-cache' : 'no-store'});
     if (!res.ok) {
       throw new Error(`HTTP Error fetching ${url}. Status: ${res.status}`);
     }
